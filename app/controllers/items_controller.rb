8a8838c66f2af2cec3bfdc8681d93ff6c7276423
atsushikitamura <ax2kx8@gmail.com>
2021-08-20 15:09:42 +0900

    バリデーション関係

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+  def new
+    @item = Item.new(item_params)
+  end
+
+  def create
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_period_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image, :name, :text, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_period_id, :price
+  end
+  validates :price, numericality: { in: 300..9999999, message: 'is out of setting range' }
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width number' }
 end
